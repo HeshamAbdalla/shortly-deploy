c6775b16d6a805a61a2bca259ca5b9b65324cf69
heshamabdalla <shamoh702@gmail.com>
2016-10-13 12:27:27 -0700

    Grunt task commit to live server

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,6 +53,26 @@ module.exports = function(grunt) {
       prodServer: {
       }
     },
+    gitpush: {
+       target: ['master'],
+      options : {
+         remote: 'live'
+      }
+    },
+    gitcommit: {
+      task: {
+        options: {
+          message: 'Grunt task commit to live server',
+          noVerify: true,
+          noStatus: false,
+          cwd: "/Users/heshamabdalla/development/hrr/week5/course.shortly-deploy.exercise"
+
+        },
+        files: {
+          src: ['app','lib','public','views', 'test','./*.js']
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -63,6 +83,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-git');
 
   grunt.registerTask('server-dev', function (target) {
     grunt.task.run([ 'nodemon', 'watch' ]);
@@ -90,6 +111,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
+     'gitcommit','gitpush'
   ]);
 
 
